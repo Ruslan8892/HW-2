0a6b70ae49ca55a586b2e74da38762862c967dd6
Ruslan8892 <hujop2021@gmail.com>
2025-01-17 20:26:21 +0200

    1

---
+++ 1.js
@@ -0,0 +1,18 @@
+let result1 = (5+3)*2-7;
+console.log(result1);
+let result2 = (10 / 2) ** 2;
+console.log(result2)
+let result3 = 2 * (4 + 3) - (8 / 2);
+console.log(result3);
+let result4 = (15 % 4) + 5;
+console.log(result4);
+let result5 = 3 - (2 ** 4);
+console.log(result5);
+let number3 = 3;
+let number4 = 4;
+console.log(number3 + number4);
+console.log(number3 - number4);
+console.log(number3 * number4);
+console.log(number3 / number4);
+console.log(number3 % number4);
+console.log(number3 ** number4);
